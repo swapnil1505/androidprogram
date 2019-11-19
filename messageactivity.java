53ea4bbbbd163e38693c73e53530ac3073dbf398
swapnil1505 <54595442+swapnil1505@users.noreply.github.com>
2019-11-19 12:59:36 +0530

    Create messageactivity.java

---
+++ messageactivity.java
@@ -0,0 +1,20 @@
+package com.example.layoutusesinandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MessageActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+
+    }
+}
